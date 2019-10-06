4ab4447eaf0cca5a331cbcd41be97866869d9a60
frisbro <felix@risbro.com>
2019-10-06 18:02:31 +0200

    added data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+data = {"keys":[ {"c":[
+    {"major":[
+      {"cdur": "Tonic"},
+      {"fdur": "dominant"},
+    ]},
+    {"minor":[
+      {"cminor": "Tonic"},
+    ]},
+  ]},
+  {"c#":[
+    {"major":[
+      {"c#dur": "Tonic"},
+      {"f#dur": "dominant"},
+    ]},
+    {"minor":[
+      {"cminor": "Tonic"},
+    ]},
+  ]}
+
+]};
